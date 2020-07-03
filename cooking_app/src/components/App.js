56d10b3bce2f4682cb7ad1265f5cb363809ac52a
Lukas Mark Jakobsen <109243@RTS-365.dk>
2020-07-03 12:23:15 +0200

    finish cooking with react + explanation

--- cooking_app/src/components/App.js
+++ cooking_app/src/components/App.js
@@ -27,7 +27,8 @@ function App() {
   const recipeContextValue = {
     handleRecipeAdd,
     handleRecipeDelete,
-    handleRecipeSelect
+    handleRecipeSelect,
+    handleRecipeChange
   }
 
   function handleRecipeSelect(id){
@@ -37,20 +38,32 @@ function App() {
   function handleRecipeAdd(){
       const newRecipe = {
         id: uuidv4(),
-        name: 'New',
+        name: '',
         servings: 1,
-        cookTime: '1:00',
+        cookTime: '',
         instructions: 'Instr.',
         ingredients:[
-          { id: uuidv4(), name: 'Name', amount: '1 Tbs' }
+          { id: uuidv4(), name: '', amount: '' }
         ]
       }
+      setSelectedRecipeId(newRecipe.id)
       setRecipes([...recipes, newRecipe])
   }
 
+  function handleRecipeChange(id, recipe){
+    const newRecipes = [...recipes]
+    const index = newRecipes.findIndex(r => r.id === id)
+    newRecipes[index] = recipe
+    setRecipes(newRecipes)
+  }
+
   function handleRecipeDelete(id){
+    if (selectedRecipeId != null && selectedRecipeId === id){
+      setSelectedRecipeId(undefined)
+    }
     setRecipes(recipes.filter(recipe=> recipe.id !== id))
   }
+  
 
   return (
     <RecipeContext.Provider value={recipeContextValue}>
